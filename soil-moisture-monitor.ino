56574e78dd3473e435468fd7f6babb5e5b970c4c
Paweł Cyło <pawel@firma.xin.pl>
2016-10-04 12:01:50 +0200

    Update docs

--- soil-moisture-monitor.ino
+++ soil-moisture-monitor.ino
@@ -105,7 +105,7 @@ void printProgressBar(int percentage) {
 }
 
 /*
- * Flash the backlight two times rapidly.
+ * Flashes the backlight two times rapidly.
  */ 
 void flashScreen() {
   for(int i = 0; i< 2; i++) {
